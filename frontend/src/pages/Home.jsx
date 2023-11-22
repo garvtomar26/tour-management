6ac10dfbac91af7beb89d57b070b21c03bc8fc71
Garv Tomar <garvtomar26@gmail.com>
2023-11-22 20:51:28 +0530

    Update Home.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -16,12 +16,16 @@ const Home = () => {
       <Row>
         <Col lg='6'>
           <div className="hero__content">
-            <div className="hero__subtitle d-flex align-items-center ">
+            <div className="hero__subtitle d-flex align-items-center">
               <Subtitle subtitle={'Know Before You Go'}/>
               <img src={worldImg} alt='' />
             </div>
-            <h1>Traveling opens the door to creating <span className="highlight"> memories</span></h1>
+            <h1>Traveling opens the door to creating <span className="highlight"> Memories</span></h1>
             <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit.
               Lorem ipsum, dolor sit amet consectetur adipisicing elit.
               Lorem ipsum, dolor sit amet consectetur adipisicing elit.
               Lorem ipsum, dolor sit amet consectetur adipisicing elit.
