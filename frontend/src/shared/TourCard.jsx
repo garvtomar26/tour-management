31a0a8142960bd034ca501b3f6370782daee6da9
Garv Tomar <garvtomar26@gmail.com>
2023-11-29 17:24:03 +0530

    53.55

--- frontend/src/shared/TourCard.jsx
+++ frontend/src/shared/TourCard.jsx
@@ -25,7 +25,7 @@ const TourCard = ({tour}) => {
         </div>
         <h5 className="tour__title"><Link to={`/tours/${id}`}>{title}</Link></h5>
         <div className="card__bottom d-flex align-items-center justify-content-between mt-3">
-            <h5></h5>
+            <h5>${price} <span> /per person</span></></h5>
         </div>
       </CardBody>      
 
