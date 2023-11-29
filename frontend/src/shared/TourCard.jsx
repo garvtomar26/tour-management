97add24b259ae25ad9fcd9aee6f898b7b026936c
Garv Tomar <garvtomar26@gmail.com>
2023-11-29 18:16:37 +0530

    58.52

--- frontend/src/shared/TourCard.jsx
+++ frontend/src/shared/TourCard.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import {Card, CardBody} from "reactstrap";
 import {Link} from "react-router-dom";
 
+import "./tour-card.css";
+
 const TourCard = ({tour}) => {
   const {id, title, photo, city, price, featured, avgRating, reviews} = tour;
   return (
@@ -11,8 +13,9 @@ const TourCard = ({tour}) => {
           <img src={photo} alt="tour-img" />
           <span>Featured</span>
         </div>
-      </Card>
-      <CardBody>
+
+
+        <CardBody>
         <div className="card__top d-flex align-items-center justify-content-between">
             <span className='tour__location d-flex align-items-center gap-1'>
                 <i class="ri-map-pin-line"></i> {city}
@@ -21,14 +24,25 @@ const TourCard = ({tour}) => {
                 <i class="ri-star-fill"></i> {avgRating}
                 <span>({reviews.length})</span>
             </span>
+          </div>
 
-        </div>
-        <h5 className="tour__title"><Link to={`/tours/${id}`}>{title}</Link></h5>
-        <div className="card__bottom d-flex align-items-center justify-content-between mt-3">
-            <h5>${price} <span> /per person</span></></h5>
-        </div>
-      </CardBody>      
+          <h5 className="tour__title"> 
+            <Link to={`/tours/${id}`}> {title}</Link> 
+          </h5>
 
+          <div className="card__bottom d-flex align-items-center justify-content-between mt-3">
+            <h5>
+              ${price} <span> /per person</span> 
+            </h5>
+
+            <button className="btn booking__btn">
+            <Link to={`/tours/${id}`}> Book Now </Link> 
+            </button>
+          </div>
+      </CardBody>
+      </Card>
+
+            
     </div>
   );
 };
