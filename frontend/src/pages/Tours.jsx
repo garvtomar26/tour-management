f03d5dc4ef7e11b098a6de5936ab517a4a0c9c03
Garv Tomar <garvtomar26@gmail.com>
2023-11-29 19:34:41 +0530

    6.19 PART 2

--- frontend/src/pages/Tours.jsx
+++ frontend/src/pages/Tours.jsx
@@ -1,11 +1,38 @@
 import React from 'react';
 import CommonSection from '../shared/CommonSection';
 
+import '../styles/tour.css';
+import tourData from '../assets/data/tours'
+import TourCard from './../shared/TourCard';
+import SearchBar from './../shared/SearchBar'
+import Newsletter from './../shared/Newsletter'
+import { Container, Row, Col } from 'reactstrap';
+
 const Tours = () => {
   return (
     <>
-      <CommonSection title = {"All Tours"} />
-  
+      <CommonSection title={"All Tours"} />
+      <section>
+        <Container>
+          <Row>
+            <SearchBar />
+          </Row>
+        </Container>
+      </section>
+      <section className='pt-0'>
+        <Container>
+          <Row>
+            {tourData?.map(tour => (
+                <Col lg='3' className='mb-4' key={tour.id}> 
+                  <TourCard tour={tour} /> 
+                </Col> 
+            ))}
+
+            <Col lg='12'>
+            </Col>
+          </Row>
+        </Container>
+      </section>
     </>
   );
 };
