dd6e19d473a1f5a0a9d25c12ecc3b1ed132d83e8
Garv Tomar <garvtomar26@gmail.com>
2023-12-01 01:50:37 +0530

    25-09 Part 4

--- frontend/src/shared/TourCard.jsx
+++ frontend/src/shared/TourCard.jsx
@@ -6,7 +6,7 @@ import calculateAvgRating from "../utils/avgRating";
 import "./tour-card.css";
 
 const TourCard = ({ tour }) => {
-  const { id, title, photo, city, price, featured, reviews } = tour;
+  const { _id, title, photo, city, price, featured, reviews } = tour;
 
   const { totalRating, avgRating } = calculateAvgRating(reviews);
 
@@ -34,7 +34,7 @@ const TourCard = ({ tour }) => {
           </div>
 
           <h5 className="tour__title">
-            <Link to={`/tours/${id}`}> {title}</Link>
+            <Link to={`/tours/${_id}`}> {title}</Link>
           </h5>
 
           <div className="card__bottom d-flex align-items-center justify-content-between mt-3">
@@ -43,7 +43,7 @@ const TourCard = ({ tour }) => {
             </h5>
 
             <button className="btn booking__btn">
-              <Link to={`/tours/${id}`}> Book Now </Link>
+              <Link to={`/tours/${_id}`}> Book Now </Link>
             </button>
           </div>
         </CardBody>
