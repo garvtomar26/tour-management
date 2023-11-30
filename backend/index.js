c35aa6646faaf472197f353e46107c73d86b2e9a
Garv Tomar <garvtomar26@gmail.com>
2023-12-01 02:48:18 +0530

    59.20 part 3

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,7 @@ import mongoose from "mongoose";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 
+import authRoute from "./routes/auth.js";
 import tourRoute from "./routes/tours.js";
 import userRoute from "./routes/users.js";
 
@@ -28,6 +29,7 @@ const connect = async () => {
 app.use(express.json());
 app.use(cors());
 app.use(cookieParser());
+app.use("/auth", authRoute);
 app.use("/tours", tourRoute);
 app.use("/users", userRoute);
 
